a1a3b185620fab318e0ccda25ac742b0e809a979
Mark Koh <mkoh@rjmetrics.com>
2015-11-24 06:02:17 +0000

    Cleaned up align_folder.

--- align_folder.py
+++ align_folder.py
@@ -3,6 +3,7 @@ from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer
 import argparse
 import os
+import pprint
 
 from lilo import Lilo
 
@@ -45,69 +46,7 @@ def main():
 
             print "Fingerprinting of {0} complete.".format(filename)
 
-    print alignment_data
-
-
-
-def main_old():
-    # Set up our commang line arguments
-    parser = argparse.ArgumentParser(description="Take in a folder of video and/or audio files and find the alignment"
-                                                 "of them all.")
-    parser.add_argument("folder",type=str,help="The folder containing your audio and video files, relative to the current directory.")
-    args = parser.parse_args()
-
-    # Our DB settings
-    config = {
-        "database": {
-            "host": "127.0.0.1",
-            "user": "root",
-            "passwd": "root",
-            "db": "dejavu"
-        },
-        "multiple_match": True
-    }
-
-
-    # Get the files in our folder
-    dir = os.path.expanduser(args.folder)
-    files = os.listdir(dir)
-
-
-    # Set up our dejavu instance
-    djv = Dejavu(config)
-
-
-    # Generate our corpus name - we'll add this functionality later.
-    corpus = dir.replace(" ","")
-    corpus = corpus.lower()
-
-    # For now, let's just empty the DB before the experiment
-    djv.db.empty()
-
-    # Iterate through the files
-    for filename in files:
-        full_path = os.path.join(dir,filename)
-
-        # For now we'll assume all the files are valid audio or video
-        if (os.path.isfile(full_path)):
-
-            print "Attempting to match {0}...".format(filename)
-
-            # Try to match the song to the existing database
-            songs = djv.recognize(FileRecognizer, full_path)
-
-            if songs:
-                for song in songs:
-                    print song
-            else:
-                print "No matches found."
-
-            print "Adding {0} to database...".format(filename)
-
-            # Now let's add this song to the DB
-            djv.fingerprint_file(full_path)
-
-            print "Added {} fingerprints to db.".format(djv.db.get_num_fingerprints())
+    pprint.pprint(alignment_data)
 
 
 if __name__ == '__main__':
