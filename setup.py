ce1d29cf5e1afc7a7135f1097d051a06d95fdfb4
Drew Banin <drew@8tracks.com>
2016-01-12 01:58:02 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -14,23 +14,10 @@ def parse_requirements(requirements):
         reqs = filter((lambda x: x), nocomments)
         return reqs
 
-PACKAGE_NAME = "PyDejavu"
-PACKAGE_VERSION = "0.1.3"
-SUMMARY = 'Dejavu: Audio Fingerprinting in Python'
-DESCRIPTION = """
-Audio fingerprinting and recognition algorithm implemented in Python
-
-See the explanation here: 
-
-`http://willdrevo.com/fingerprinting-and-audio-recognition-with-python/`__
-
-Dejavu can memorize recorded audio by listening to it once and fingerprinting 
-it. Then by playing a song and recording microphone input or on disk file, 
-Dejavu attempts to match the audio against the fingerprints held in the 
-database, returning the song or recording being played.
-
-__ http://willdrevo.com/fingerprinting-and-audio-recognition-with-python/
-"""
+PACKAGE_NAME = "lilo"
+PACKAGE_VERSION = "0.1"
+SUMMARY = 'lilo'
+DESCRIPTION = ""
 REQUIREMENTS = parse_requirements("requirements.txt")
 
 setup(
@@ -38,23 +25,16 @@ setup(
     version=PACKAGE_VERSION,
     description=SUMMARY,
     long_description=DESCRIPTION,
-    author='Will Drevo',
-    author_email='will.drevo@gmail.com',
-    maintainer="Will Drevo",
-    maintainer_email="will.drevo@gmail.com",
-    url='http://github.com/tuxdna/dejavu',
+    author='Mark Koh',
+    author_email='',
+    maintainer="",
+    maintainer_email="",
+    url='https://github.com/projectjamjar/lilo',
     license='MIT License',
     include_package_data=True,
     packages=find_packages(),
     platforms=['Unix'],
     install_requires=REQUIREMENTS,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    keywords="python, audio, fingerprinting, music, numpy, landmark",
+    classifiers=[],
+    keywords="",
 )
